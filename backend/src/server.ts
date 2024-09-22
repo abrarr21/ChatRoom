dad02d945e278d5cf22b43a60dd9695319fb22a4
abrarr21 <abrarrr2110@gmail.com>
2024-09-22 18:17:20 +0530

    auth/prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,14 @@
 import express from "express";
-const app = express();
+import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+const app = express();
+dotenv.config();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.get("/", (_req, res) => {
   res.send("Server built");
 });
@@ -9,6 +17,6 @@ app.get("/user", (_req, res) => {
   res.send("User 1 mounted");
 });
 
-app.listen(6969, () => {
-  console.log("Server is running at Port:6969");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running at Port: ${process.env.PORT}`);
 });
