0253b8a4930e51081bb56bdfacf8f24545f895b6
abrarr21 <abrarrr2110@gmail.com>
2024-09-23 21:03:05 +0530

    auth & middleware

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -63,6 +63,7 @@ export const signup = async (req: Request, res: Response) => {
       res.status(400).json({ error: "Invalid User data" });
     }
   } catch (err: any) {
+    console.log("Error in Signup Controller", err.message);
     if (err instanceof z.ZodError) {
       return res.status(400).json({
         errors: err.errors,
@@ -72,10 +73,85 @@ export const signup = async (req: Request, res: Response) => {
   }
 };
 
-export const login = (req: Request, res: Response) => {
-  res.send("login");
+const loginSchema = z.object({
+  username: z.string().min(3, "Username must be at lest 3 character long"),
+  password: z.string().min(3, "Password must be at least 6 character long"),
+});
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const parsedData = loginSchema.parse(req.body);
+
+    //Find the user in the database by username
+    const user = await prisma.user.findUnique({
+      where: {
+        username: parsedData.username,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid Credentials" });
+    }
+
+    const isPasswordCorrect = await bcryptjs.compare(
+      parsedData.password,
+      user.password,
+    );
+
+    if (!isPasswordCorrect) {
+      res.status(400).json({ error: "Invalid Credentials" });
+    }
+
+    //Generate token and send cookie to the client
+    generateToken(user.id, res);
+
+    res.status(200).json({
+      id: user.id,
+      fullname: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error: any) {
+    console.log("Error in login Controller", error.message);
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ errors: error.errors });
+    }
+    res.status(500).json({ error: "Internal Server error" });
+  }
 };
 
-export const logout = (req: Request, res: Response) => {
-  res.send("logout");
+export const logout = async (_req: Request, res: Response) => {
+  try {
+    res.cookie("jwt-token", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error: any) {
+    console.log("Error in logout Controller", error.message);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
+};
+
+export const getMe = async (req: Request, res: Response) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id: req.user.id } });
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      id: user.id,
+      fullname: user.fullname,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error: any) {
+    console.log("Error in getMe controller", error.message);
+    res.status(500).json({
+      error: "Internal Server Error",
+    });
+  }
 };
