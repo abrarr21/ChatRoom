ade8d696c58081fe58c21ed91ed93acc89f3853a
abrarr21 <abrarrr2110@gmail.com>
2024-10-03 21:31:55 +0530

    shimmerUI added

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,10 +1,13 @@
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
   const { loading, messages } = useGetMessages();
   return (
     <div className="px-4 flex-1 overflow-auto">
+      {loading && [...Array(5)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
       {!loading &&
         messages.map((message) => (
           <Message key={message.id} message={message} />
