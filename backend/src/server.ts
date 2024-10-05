8af887cc2465756300541fd26fbabd3cdd4bdd84
abrarr21 <abrarrr2110@gmail.com>
2024-10-05 12:49:12 +0530

    deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -8,11 +9,20 @@ import { server, app } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (_req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(process.env.PORT, () => {
   console.log(`Server is running at Port: ${process.env.PORT}`);
