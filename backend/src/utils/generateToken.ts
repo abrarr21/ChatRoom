8f68c4c1a39f9b7197482bded0495b48865a6fe1
abrarr21 <abrarrr2110@gmail.com>
2024-10-06 10:57:10 +0530

    secure option

--- backend/src/utils/generateToken.ts
+++ backend/src/utils/generateToken.ts
@@ -10,7 +10,7 @@ const generateToken = (userId: string, res: Response) => {
     maxAge: 15 * 24 * 60 * 60 * 1000, //millisecond
     httpOnly: true,
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV === "production",
   });
 
   return token;
