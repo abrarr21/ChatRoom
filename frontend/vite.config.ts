9620c7ff92bd75ad267fdbaa865e3960112ec617
abrarr21 <abrarrr2110@gmail.com>
2024-10-16 16:29:36 +0530

    Dockerized app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:6969",
+        target: process.env.DOCKER
+          ? "http://backend:6969"
+          : process.env.VITE_API_BASE_URL,
+        changeOrigin: true,
+        secure: false,
       },
     },
   },
